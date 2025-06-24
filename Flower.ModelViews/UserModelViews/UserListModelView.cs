44a5f14fdeba1fc65335e3b44ca71b94a4fed33e
Thang Lam <thanglam271003@gmail.com>
2025-06-25 01:08:27 +0700

    update get user and admin

---
+++ Flower.ModelViews/UserModelViews/UserListModelView.cs
@@ -0,0 +1,23 @@
+﻿using Flower.ModelViews.RoleModelViews;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flower.ModelViews.UserModelViews
+{
+    public class UserListModelView
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string? FullName { get; set; }
+        public int? Age { get; set; }
+        public string? AvatarUrl { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public int? Status { get; set; }
+        public DateTimeOffset CreatedTime { get; set; }
+        public DateTimeOffset LastUpdatedTime { get; set; }
+        public RoleModelView Role { get; set; }
+    }
+}
